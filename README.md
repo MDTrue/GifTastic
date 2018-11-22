49ac789e8f8af9818561b32f11679a9f17a00d13
MDTrue <MDTrueII@hotmail.com>
2018-11-21 18:26:01 -0800

    style added, readme added, bugs fixed, sized for screens

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+#Giftastic
+=======
+
+## For my friend Jason
+
+GIFs make my think of my friend Jason who loves to text these out to all our friends.
+So I made this page for him
+
+![Game Image](assets\images\giftastic.png)
+
+--- api queried with AJAX---
+
+Made with:
+
+  * html
+  * css
+  * bootstrap
+  * javascript
+  * jquery
+
 # GifTastic
--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,25 @@
 //GLOBAL VARIABLES
 
-// HELPER FUNCTIONS
+//HELPER FUNCTIONS    Basic outline to keep in mind
 
 //EVENT LISTENERS
 
 
 //list of variables for pre loaded buttons
-var jSearches = ["Hey girl", "fabulous", "totally", "happy hour", "rainbow", "happy dance", "so wrong", "bye felicia", "time to go", "swedes"]
+var jSearches = ["random", "trending", "Hey girl", "fabulous","eurovision","Netta", "totally","sverige","happy hour", "rainbow", "happy dance","tequila" ,"so wrong", "bye felicia", "time to go",
+    "swedes","mazel tov", "mmhmmm", "gurl","you nasty","byeee"]
 // page load check and load buttons
 $(document).ready(function () {
-
+    //load the page full of "unicorn" GIFs...this wasn't part of the assignment but I prefer the look this way. its easy to remove.
+    $.ajax({
+        url: queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "unicorn" +
+            "&api_key=dc6zaTOxFJmzC",
+        method: "GET"
+    }).then(function (response) {
+        // console.log(response);
+        display(response)
+        //loading the pre-loaded buttons for the variables above
+    })
     for (var j = 0; j < jSearches.length; j++) {
         var proto = $("<button>");
         proto.addClass("buttons");
@@ -18,30 +28,23 @@ $(document).ready(function () {
         // console.log(proto)
         $("#buttonRow").append(proto)
     }
+    //what happens when the search button is used
     $("#searchbutton").on("click", function (event) {
         event.preventDefault();
         // console.log("click")
         var searchWord = $("#searchField").val().trim()
-        getGiphy(searchWord)
+        getGiphy(searchWord);
+        $("#searchField").val("")
+
     })
     //if loaded button pushed, define variable, call search function
     //Event delegation
-    // $(document).on("click",".button", function (event) {
     $(".buttons").on("click", function (event) {
         event.preventDefault();
-        // var searchWord = $("#searchField").val().trim()\
         var searchWord = $(this).attr("data-name")
         // console.log(searchWord)
         getGiphy(searchWord)
         jSearches.push(searchWord)
-
-        //if search button pushed, define search variable,push button to array, call search function
-        // $(".button").on("click", function (event) {
-        //     event.preventDefault();
-        //     console.log("click")
-        //     var searchWord = $("#searchField").val().trim()
-        //     
-        //     getGiphy(searchWord)
 
     })
     //search giphy function (AJAX and 'then' function)
@@ -58,57 +61,54 @@ $(document).ready(function () {
 
         })
     }
-        //display giphy function, clear div first, iterate through 10, store animated versions for later
-        function display(response) {
-            $("#giphyGallery").empty()
-            var results = (response.data);
-            for (var j = 0; j < 10; j++) {
-                if (results[j].rating !== "r") {
-                    var rating = results[j].rating;
-                    // console.log(rating)
-                    var p = $("<p>").text(rating)
-                    var galleryDiv = $("<div>");
-                    var galGif = $("<img>");
-                    galGif.attr({
-                        "src": results[j].images.fixed_height_still.url,
-                        "data-still": results[j].images.fixed_height_still.url,
-                        "data-animate": results[j].images.fixed_height.url,
-                        "data-state": "still"
-                    })
-
-                    galGif.addClass("movGif")
-                    console.log(galGif)
-                    galleryDiv.append(galGif);
-                    galleryDiv.append(p);
-                    $("#giphyGallery").prepend(galleryDiv)
-                }
+    //display giphy function, clear div first, iterate through 10, store animated versions for later
+    function display(response) {
+        $("#giphyGallery").empty()
+        var results = (response.data);
+        for (var j = 0; j < 10; j++) {
+            if (results[j].rating !== "r") {
+                var rating = results[j].rating;
+                // console.log(rating)
+                var p = $("<p>").text("rated: " + rating)
+                var galleryDiv = $("<div class = 'galDiv'>");
+                var galGif = $("<img>");
+                galGif.attr({
+                    "src": results[j].images.fixed_height_still.url,
+                    "data-still": results[j].images.fixed_height_still.url,
+                    "data-animate": results[j].images.fixed_height.url,
+                    "data-state": "still"
+                })
+                //push to page galler div
+                galGif.addClass("movGif")
+                console.log(galGif)
+                galleryDiv.append(galGif);
+                galleryDiv.append(p);
+                $("#giphyGallery").prepend(galleryDiv)
             }
-        };
-        $(document).on('click', '.movGif', function(){
-        // function animateGif(){
-        // $(".movGif").on("click", function() {
-            console.log("click")
-                var state = $(this).attr("data-state"); 
-                
-                if (state === "still") {
-                    
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate")
-                    
-                }
-                else {
-                    
-                    
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still")
-                }
-            })
-            
-            
-        //     $(document).on("click", ".galGif", animateGif);
-        })
-        
+        }
+    };
+    //make the gifs animate on click
+    $(document).on('click', '.movGif', function () {
+        console.log("click")
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate")
+
+        }
+        else {
+
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still")
+        }
+    })
 
 
+})
 
-    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=rainbow&api_key=dc6zaTOxFJmzC";
+
+//console.logs left inline at good testpoints for debugging
+// var queryURL = "https://api.giphy.com/v1/gifs/search?q=rainbow&api_key=dc6zaTOxFJmzC"; <---saving my api code
